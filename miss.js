c604652b0757057c60e9dcbf4d9e6a5198973362
dreadlinx <tinobafana39@gmail.com>
2025-07-24 21:06:22 +0200

    i am a pro at this

---
+++ miss.js
@@ -0,0 +1,3 @@
+console.log("Hello, world!");
+// This is a simple JavaScript file that logs a message to the console.
+// You can run this file using Node.js or in a web browser console.
---
+++ sir.py
@@ -0,0 +1,5 @@
+print("Hello, world!")
+# This is a simple Python script that prints a message to the console.
+# You can run this script using Python interpreter.
+# To run this script, save it as sir.py and execute it with the command:
+# python sir.py
